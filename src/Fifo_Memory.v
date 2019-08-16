efc4bf60860ff333201dfb7bc01b97e821737261
yasnakateb <37741028+yasnakateb@users.noreply.github.com>
2019-08-17 03:09:57 +0430

    Updated: Fifo_Memory

--- src/Fifo_Memory.v
+++ src/Fifo_Memory.v
@@ -30,52 +30,50 @@ module Fifo_Memory(
   wire [DATA_WIDTH-1:0] buffer_Output;
   wire [BUFFER_WIDTH-1:0] counter;         
 
-
-
-Write_Interface #(.BUFFER_WIDTH(3))write_interface(
-  .clock(clock),
-  .reset(reset),
-  .write_Enable(write_Enable),
-  .sig_Full(sig_Full),
-  .write_Pointer(write_Pointer)
-  );
-
-
-Memory_Array #(.BUFFER_WIDTH(3),
-               .DATA_WIDTH(8),
-               .BUFFER_SIZE(8))
-  memory_array(
-    .clock(clock),
-    .write_Enable(write_Enable),
-    .write_Pointer(write_Pointer),
-    .sig_Full(sig_Full),
-    .read_Pointer(read_Pointer),
-    .buffer_Input(buffer_Input),
-    .buffer_Output(buffer_Output)
-  );
-
-
-Read_Interface #(.BUFFER_WIDTH(3))read_interface(
-  .clock(clock),
-  .reset(reset),
-  .read_Enable(read_Enable),
-  .sig_Empty(sig_Empty),
-  .read_Pointer(read_Pointer)
-  ); 
-
-
-Compare_Logic #(.BUFFER_WIDTH(3),
-               .BUFFER_SIZE(8))
-  compare_logic(
+  Write_Interface #(.BUFFER_WIDTH(3))write_interface(
     .clock(clock),
     .reset(reset),
-    .write_Pointer(write_Pointer),
-    .read_Pointer(read_Pointer),
     .write_Enable(write_Enable),
-    .read_Enable(read_Enable),
     .sig_Full(sig_Full),
+    .write_Pointer(write_Pointer)
+    );
+
+
+  Memory_Array #(.BUFFER_WIDTH(3),
+                 .DATA_WIDTH(8),
+                 .BUFFER_SIZE(8))
+    memory_array(
+      .clock(clock),
+      .write_Enable(write_Enable),
+      .write_Pointer(write_Pointer),
+      .sig_Full(sig_Full),
+      .read_Pointer(read_Pointer),
+      .buffer_Input(buffer_Input),
+      .buffer_Output(buffer_Output)
+    );
+
+
+  Read_Interface #(.BUFFER_WIDTH(3))read_interface(
+    .clock(clock),
+    .reset(reset),
+    .read_Enable(read_Enable),
     .sig_Empty(sig_Empty),
-    .counter(counter)
-  );
+    .read_Pointer(read_Pointer)
+    ); 
+
+
+  Compare_Logic #(.BUFFER_WIDTH(3),
+                 .BUFFER_SIZE(8))
+    compare_logic(
+      .clock(clock),
+      .reset(reset),
+      .write_Pointer(write_Pointer),
+      .read_Pointer(read_Pointer),
+      .write_Enable(write_Enable),
+      .read_Enable(read_Enable),
+      .sig_Full(sig_Full),
+      .sig_Empty(sig_Empty),
+      .counter(counter)
+    );
 
 endmodule  
